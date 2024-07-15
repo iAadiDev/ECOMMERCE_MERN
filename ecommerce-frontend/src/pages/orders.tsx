c6a53cb550a9cc68fa0d9ecececdc4bbe54171af
Aaditya Kediyal <contactaadityakediyal@gmail.com>
2024-07-15 12:59:03 +0530

    15Jul2024

--- ecommerce-frontend/src/pages/orders.tsx
+++ ecommerce-frontend/src/pages/orders.tsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import TableHOC from '../components/admin/TableHOC';
+
+type DataType ={
+_id:string;
+amount:number;
+quantity:number,
+discount:number;
+status:ReactElement;
+action:ReactElement;
+};
+
+const column:Column<DataType>[]=[
+  {
+    Header:"ID",
+    accessor:"-id"
+  },
+  {
+    Header:"Quantity",
+    accessor:"quantity",
+  },
+  {
+    Header:"Discount",
+    accessor:"discount",
+  },
+  {
+    Header:"Amount",
+    accessor:"amount",
+  },
+  {
+    Header:"Status",
+    accessor:"status",
+  },
+  {
+    Header:"Action",
+    accessor:"action",
+  }
+  
+];
 
 const Orders = () => {
+
+const [rows]= useState<DataType[]>([
+  {_id: "asdf",
+  amount: 4534,
+  quantity:20,
+  discount:123,
+  status:<span className='red'>
+    Procesing
+  </span>,
+  action:<link to={`/order/asdf`}>View</link>,
+  },
+]);
+
+const Table =TableHOC<DataType>(column,rows,"dashboard-product-box","orders",true)();
+
   return (
     <div className='container'>
       Orders Page
       <h1>My Orders</h1>
+      {Table}
     </div>
   )
 };
